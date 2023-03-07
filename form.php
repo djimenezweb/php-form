34e1d65ab63986f60dfa08eeae5b74f1c01a2fac
Daniel Jiménez <djimenezweb@gmail.com>
2023-03-07 18:55:16 +0100

    updated

--- form.php
+++ form.php
@@ -1,15 +1,16 @@
 <?php
 // Campos del formulario
 $name = $_POST['name'];
+$mensaje = $_POST['mensaje'];
 
 // Correo electrónico al que se envía el formulario
 $from = "info@djimenezweb.com";
 $to = "djimenezweb@gmail.com";
 $subject = "Formulario enviado por correo";
-$message = "Texto bla bla bla" . $name
+$message = "Texto bla bla bla" . $name . "Y el mensaje es" . $mensaje;
 $headers = "From:" . $from;
 
-@mail($to, $subject, $message, $headers)
+@mail($to, $subject, $message, $headers);
 // A qué página se redirige una vez se ha enviado el formulario:
-header('Location: index.html')
+header('Location: index.html');
 ?>
